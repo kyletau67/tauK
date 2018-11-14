c0bf0009e8c180b2f1776b8e7c0b4565257f1e4c
Kyle Tau <kyletau2@gmail.com>
2018-11-13 22:34:47 -0500

    k24 up

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,11 +1,17 @@
+'''Kyle Tau
+SoftDev1 pd6
+K24 -- A RESTful Journey Skyward
+2018-11-13'''
+
 from flask import Flask,render_template
-import urrlib2, json
+import urllib, json
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def go():
-    urllib2.urlopen('https://api.nasa.gov/planetary/apod?api_key=motrt4z0moLTYLk3ziqhyD8KCZsf6k4n8EwJcBN0')
-    return render_template('page.html', pic = ???)
+    r = urllib2.urlopen('https://api.nasa.gov/planetary/apod?api_key=motrt4z0moLTYLk3ziqhyD8KCZsf6k4n8EwJcBN0').read()
+    dict = json.loads(r)
+    return render_template('page.html', pic = r['url'])
 
 if __name__ == "__main__":
     app.debug = True
